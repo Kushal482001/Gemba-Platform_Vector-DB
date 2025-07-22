04a03b86cf6b6908fee9f8429a6f0f704b3a54eb
Kushal482001 <78710611+Kushal482001@users.noreply.github.com>
2025-07-22 16:58:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,8 @@ def load_xlsx_as_db(filepath):
     chunks = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0).split_documents(docs)
 
     embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs={"device": "cpu"}
     )
     return FAISS.from_documents(chunks, embeddings)
 
